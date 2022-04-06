6276da17b361c70303829b76f52459fc137bb570
david-aguiyi <davidaguiyi9@gmail.com>
2022-04-06 10:47:19 +0100

    Initialize keys for drum-machine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
 import React from "react";
 
-const App = () => <div>Hello</div>;
-console.log("hello world");
+class App extends React.Component {
+  state = {
+    keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
+  };
+  render() {
+    return (
+      <div id="drum-machine">
+        <div id="display">
+          {keys.map((key, idx) => (
+            <Box text={key} key={idx} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+const app = () => <div>Hello</div>;
+
+const Box = () => <div className="box">{props.text}</div>;
 export default App;
