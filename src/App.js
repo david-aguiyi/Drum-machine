ebb6df8e1f06705b90276e1eddee12e28aaedcda
david-aguiyi <davidaguiyi9@gmail.com>
2022-04-06 20:56:41 +0100

    destructure code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
 import React from "react";
 
-class App extends React.Component {
-  state = {
-    keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
-  };
-  render() {
-    console.log("htell");
-    const { keys } = this.state;
-    return (
-      <div id="drum-machine">
-        <div id="display" className="display">
-          {keys.map((key, idx) => (
-            <Box text={key} key={idx} />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
+const App = (props) => (
+  <div id="display" className=" m-auto display">
+    {keys.map((key, idx) => (
+      <Box text={key} key={idx} />
+    ))}
+  </div>
+);
 
 const Box = (props) => <div className="box">{props.text}</div>;
 
