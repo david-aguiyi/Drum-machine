512cd1d92fdfd5766dea2d27c4b0ab4068835785
david-aguiyi <davidaguiyi9@gmail.com>
2022-04-08 01:10:42 +0100

    Add array of sound links and keys

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 import "./App.css";
 import React from "react";
 
+const sounds = [
+  {
+    key: "Q",
+    song: "Heater-1",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    key: "W",
+    song: "Heater-2",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    key: "E",
+    song: "Heater-3",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    key: "A",
+    song: "Heater-4",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    key: "S",
+    song: "Heater-6",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    key: "D",
+    song: "Dsc_Oh",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    key: "Z",
+    song: "Kick_n_Hat",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    key: "X",
+    song: "RP4_KICK_1",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    key: "C",
+    song: "Cev_H2",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
 const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+const sound = [
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
 
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+];
 const App = (props) => (
   <div id="display" className="container display ">
     {keys.map((key, idx) => (
@@ -13,7 +72,12 @@ const App = (props) => (
 
 class Box extends React.Component {
   render() {
-    return <div className="box">{this.props.text}</div>;
+    return (
+      <div className="box">
+        {this.props.text}
+        <audio />
+      </div>
+    );
   }
 }
 
