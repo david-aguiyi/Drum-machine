59c84b1e1e5bb4bae4a1ae1ccc5b7b3df4e6f5ad
david-aguiyi <davidaguiyi9@gmail.com>
2022-04-10 20:42:34 +0100

    Create playSound function This function plays a sound anytime a key is clicked

--- src/App.js
+++ src/App.js
@@ -5,77 +5,73 @@ const sounds = [
   {
     key: "Q",
     song: "Heater-1",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
   },
   {
     key: "W",
     song: "Heater-2",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
   },
   {
     key: "E",
     song: "Heater-3",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
   },
   {
     key: "A",
     song: "Heater-4",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
   },
   {
     key: "S",
     song: "Heater-6",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
   },
   {
     key: "D",
     song: "Dsc_Oh",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
   },
   {
     key: "Z",
     song: "Kick_n_Hat",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
     key: "X",
     song: "RP4_KICK_1",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
   },
   {
     key: "C",
     song: "Cev_H2",
-    sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
   },
 ];
-const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
-const sound = [
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
 
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-];
-const App = (props) => (
+const App = () => (
   <div id="display" className="container display ">
-    {keys.map((key, idx) => (
-      <Box text={key} key={idx} />
+    {sounds.map((sound, idx) => (
+      <Box text={sound.key} key={idx} audio={sound.mp3} />
     ))}
   </div>
 );
 
 class Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.audio = React.createRef();
+  }
+  playSound = () => {
+    console.log("success");
+    this.audio.current.play();
+  };
   render() {
+    const { text, audio } = this.props;
     return (
-      <div className="box">
-        {this.props.text}
-        <audio />
+      <div className="box" onClick={this.playSound}>
+        {text}
+        <audio src={audio} ref={this.audio} className="clip" id={text} />
       </div>
     );
   }
